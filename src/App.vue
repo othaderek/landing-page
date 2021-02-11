83456e315f09b8aa65f2acfce1d0f4f9f7b74081
othaderek <otha.hernandez@gmail.com>
2021-02-10 20:44:57 -0500

    Added some components.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Links/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Links from './components/Links'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Links
   }
 }
 </script>
---
+++ src/components/Links.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <div v-for="link in linkItems" v-bind:key="link.id">
+        <LinkItem v-bind:link="link"/>
+    </div>
+</div>
+</template>
+
+<script>
+import LinkItem from './LinkItem'
+export default {
+    name: "Links",
+    components: {LinkItem},
+    data(){
+        return {
+            linkItems: [
+                {
+                    id: 1,
+                    title: "Instagram",
+                    link: "https://www.instagram.com/_odhp_"
+                },
+                                {
+                    id: 2,
+                    title: "Spotify",
+                    link: "https://distrokid.com/hyperfollow/othaderekhernandezpena/our-june"
+                },
+                {
+                    id: 3,
+                    title: "Youtube",
+                    link: "https://youtu.be/FDUKjMiDIuc"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
